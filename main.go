8e4090e5ec2aeb205112d9888c9f04cfe557edde
Josa Gesell <josa@gesell.me>
2017-01-09 14:32:48 +0100

    📦  replace --push with --do-not-push

--- main.go
+++ main.go
@@ -13,26 +13,27 @@ import (
 
 func main() {
 	usage := stringutils.StripPrefix(`
-	  Usage:
-	    git-release [--major|--minor|--patch] [--stable|--beta|--rc] [--dirty] [--force] [--no-empty-commit]
-	    git-release --stable|--beta|--rc [--dirty] [--force] [--no-empty-commit]
-	    git-release <version> [--dirty] [--force] [--no-empty-commit]
-	    git-release --help
-	    git-release --version
+    Usage:
+      git-release [--major|--minor|--patch] [--stable|--beta|--rc] [--dirty] [--force] [--do-not-push] [--no-empty-commit]
+      git-release --stable|--beta|--rc                             [--dirty] [--force] [--do-not-push] [--no-empty-commit]
+      git-release <version>                                        [--dirty] [--force] [--do-not-push] [--no-empty-commit]
+      git-release --help
+      git-release --version
 
-	  Options:
-	    -h --help          Show this screen.
-	    --version          Show version.
-	    --dirty            Include changed files in release commit.
-	    --force            Force new commit even thought the latest commit is already tagged.
+    Options:
+      -h --help          Show this screen.
+      --version          Show version.
+      --dirty            Include changed files in release commit.
+      --force            Force new commit even thought the latest commit is already tagged.
+      --do-not-push      Do not push commit and tags
       --no-empty-commit  Do not commit if nothing changed
-	`)
+  `)
 
 	arguments, _ := docopt.Parse(usage, nil, true, "Git Release 0.1.0", false)
 
 	force := arguments["--force"] == true
 	dirty := arguments["--dirty"] == true
-	push := arguments["--push"] == true
+	push := arguments["--no-empty-commit"] != true
 	noEmptyCommit := arguments["--no-empty-commit"] == true
 
 	if git.IsDirty() && !dirty {
