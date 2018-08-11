ef168fd5a6892c5000f990710decc19f5cd43bda
Josa Gesell <josa@gesell.me>
2018-08-11 20:23:57 +0200

    ⬆️  Bump versions in .podspec und .podspec.json files

--- versionfiles/versionfiles.go
+++ versionfiles/versionfiles.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"regexp"
 )
 
@@ -13,6 +14,14 @@ func UpdateAll(version string) error {
 	updateJSON("package.json", version)
 	updateJSON("bower.json", version)
 	updateJSON("composer.json", version)
+
+	for _, name := range find("*.podspec.json") {
+		updateJSON(name, version)
+	}
+
+	for _, name := range find("*.podspec") {
+		updatePodspec(name, version)
+	}
 
 	if _, err := os.Stat("./.git-release/set-version.sh"); err == nil {
 		cmd := exec.Command("./.git-release/set-version.sh", version)
@@ -39,4 +48,26 @@ func updateJSON(name string, version string) {
 	content = r.ReplaceAllString(content, "\"version\": \""+version+"\"")
 
 	ioutil.WriteFile(name, []byte(content), fileStat.Mode())
+}
+
+func updatePodspec(name string, version string) {
+
+	fileStat, err := os.Stat(name)
+	if os.IsNotExist(err) {
+		return
+	}
+
+	bytes, _ := ioutil.ReadFile(name)
+	content := string(bytes)
+
+	r, _ := regexp.Compile("(\\.version\\s*=\\s*)(['\"])[^'\"]+['\"]")
+
+	content = r.ReplaceAllString(content, "${1}${2}"+version+"${2}")
+
+	ioutil.WriteFile(name, []byte(content), fileStat.Mode())
+}
+
+func find(pattern string) []string {
+	files, _ := filepath.Glob(pattern)
+	return files
 }
